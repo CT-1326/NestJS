c86bdc3f05719e06cda50f0fcd1ca9765fc3f3fb
sejun-park <smw1326@withnetworks.com>
2023-12-22 15:02:24 +0900

    delete 추가

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,51 +1,44 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  ForbiddenException,
-  Get,
-  Param,
-  Post,
-  Put,
+    Body,
+    Controller,
+    Delete,
+    ForbiddenException,
+    Get,
+    Param,
+    Post,
+    Put
 } from '@nestjs/common';
-import { ProductsService } from './products.service';
-import { CreateProductInput } from './dto/createProduct.dto';
-import { UpdateProductInput } from './dto/updateProduct.dto';
+import {ProductsService} from './products.service';
+import {CreateProductInput} from './dto/createProduct.dto';
+import {UpdateProductInput} from './dto/updateProduct.dto';
 // import { Product } from "./interfaces/product.interface";
-import { Product } from './entities/product.entity';
+import {Product} from './entities/product.entity';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productService: ProductsService) {}
+    constructor(private readonly productService : ProductsService) {}
 
-  @Post()
-  createProduct(@Body() input: CreateProductInput): Promise<Product> {
-    return this.productService.create(input);
-  }
-
-  @Put(':id')
-  updateProduct(
-    @Param('id') id: number,
-    @Body() input: UpdateProductInput,
-  ): Promise<Product> {
-    if (id != input.id) {
-      throw new ForbiddenException();
+    @Post()
+    createProduct(@Body()input : CreateProductInput): Promise<Product> {
+        return this
+            .productService
+            .create(input);
     }
-    return this.productService.modify(input);
-  }
 
-  // @Get(':id')
-  // fetchProduct(@Param('id') id: number): Product {
-  //     return this.productService.findOne(id);
-  // }
+    @Put(':id')
+    updateProduct(@Param('id')id : number, @Body()input : UpdateProductInput,): Promise<Product> {
+        if(id != input.id) {
+            throw new ForbiddenException();
+        }
+        return this
+            .productService
+            .modify(input);
+    }
 
-  // @Get()
-  // fetchProducts(): Product[] {
-  //     return this.productService.findAll();
-  // }
-
-  // @Delete(':id')
-  // deelteProduct(@Param('id') id: number): boolean {
-  //     return this.productService.delete(id);
-  // }
+    @Delete(':id')
+    deelteProduct(@Param('id')id : number): Promise<boolean> {
+        return this
+            .productService
+            .delete(id);
+    }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,37 +1,54 @@
-import { Injectable } from '@nestjs/common';
-// import { Product } from './interfaces/product.interface';import { CreateProductInput } from './dto/createProduct.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Product } from './entities/product.entity';
-import { Repository } from 'typeorm';
-import { CreateProductInput } from './dto/createProduct.dto';
-import { UpdateProductInput } from './dto/updateProduct.dto';
+import {Injectable} from '@nestjs/common';
+// import { Product } from './interfaces/product.interface';
+import {InjectRepository} from '@nestjs/typeorm';
+import {Product} from './entities/product.entity';
+import {Repository} from 'typeorm';
+import {CreateProductInput} from './dto/createProduct.dto';
+import {UpdateProductInput} from './dto/updateProduct.dto';
 
 @Injectable()
 export class ProductsService {
-  constructor(
-    @InjectRepository(Product)
-    private readonly productRepository: Repository<Product>,
-  ) {}
+    constructor(
+        @InjectRepository(Product)private readonly productRepository : Repository<Product>,
+    ) {}
 
-  async create(input: CreateProductInput): Promise<Product> {
-    const result = await this.productRepository.save({
-      ...input,
-    });
+    async create(input : CreateProductInput): Promise<Product> {
+        const result = await this
+            .productRepository
+            .save({
+                ...input
+            });
 
-    return result;
-  }
+        return result;
+    }
 
-  async modify(input: UpdateProductInput): Promise<Product> {
-    const product = await this.productRepository.findOne({
-      where: { id: input.id },
-    });
+    async modify(input : UpdateProductInput): Promise<Product> {
+        const product = await this
+            .productRepository
+            .findOne({
+                where: {
+                    id: input.id
+                }
+            });
+        const newProduct = {
+            ...product,
+            ...input
+        };
 
-    const newProduct = {
-      ...product,
-      ...input,
-    };
+        const result = await this
+            .productRepository
+            .save(newProduct);
 
-    const result = await this.productRepository.save(newProduct);
-    return result;
-  }
+        return result;
+    }
+
+    async delete(id : number): Promise<boolean> {
+        const result = await this
+            .productRepository
+            .softDelete({id: id});
+            
+        return result.affected
+            ? true
+            : false;
+    }
 }
