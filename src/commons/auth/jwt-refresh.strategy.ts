63a42ce9f150b8031c4f814643b883a3a6f48b36
sejun-park <smw1326@withnetworks.com>
2023-12-28 14:06:21 +0900

    refresh 토큰을 통한 access 토큰 재발급

--- src/commons/auth/jwt-refresh.strategy.ts
+++ src/commons/auth/jwt-refresh.strategy.ts
@@ -1,13 +1,15 @@
-import { PassportStrategy } from "@nestjs/passport";
-import { Strategy, ExtractJwt } from "passport-jwt";
+import {PassportStrategy} from "@nestjs/passport";
+import {Strategy, ExtractJwt} from "passport-jwt";
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 export class JwtRefreshStretagy extends PassportStrategy(Strategy, 'refresh') {
     constructor() {
         super({
-            jwtFromRequest: (req)=>{
-                const cookie = req.cookies['refreshToken'];
+            jwtFromRequest: (req) => {
+                // console.log(req.headers);
+                const cookie = req.headers.cookie;
+                console.log("cookie : ", cookie);                
                 return cookie;
             },
             secretOrKey: process.env.REFRESH_TOKEN_SECRET_KEY
@@ -16,9 +18,6 @@ export class JwtRefreshStretagy extends PassportStrategy(Strategy, 'refresh') {
 
     validate(payload) {
         console.log(payload);
-        return {
-            email: payload.email,
-            id: payload.sub
-        }
+        return {email: payload.email, id: payload.sub}
     }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    Param,
+    Post,
+    UseGuards
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './entity/user.entity';
 import * as bcrypt from 'bcrypt'
@@ -8,28 +15,26 @@ import { AuthGuard } from '@nestjs/passport';
 @Controller('user')
 export class UserController {
     PASSWORD_SALT = 10;
-    constructor(
-        private readonly userService: UserService
-    ) {}
+    constructor(private readonly userService: UserService) { }
 
     @Post()
-    async createUser(@Body() input: CreateUserInput) : Promise<User> {
-        const hashedPassword = await bcrypt.hash(
-            input.password,
-            this.PASSWORD_SALT
-        );
+    async createUser(@Body() input: CreateUserInput): Promise<User> {
+        const hashedPassword = await bcrypt.hash(input.password, this.PASSWORD_SALT);
         const user = {
             ...input,
             password: hashedPassword
         };
 
-        return this.userService.create(user);
+        return this
+            .userService
+            .create(user);
     }
 
     @UseGuards(AuthGuard('access'))
     @Get(':email')
     getEmail(@Param('email') email: string): Promise<User> {
-        return this.userService.findOne(email);
+        return this
+            .userService
+            .findOne(email);
     }
-
 }
