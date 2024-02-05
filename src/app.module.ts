39093305d2c4ee07d6a0c23f5b5a57b3ad46f590
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2024-02-05 18:28:49 +0900

    typeORM을 이용한 mariaDB 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/TypeOrmConfigService';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
