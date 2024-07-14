768b002660f69a1570fdb95e913e507c96aec889
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2024-07-14 22:28:16 +0900

    환경변수를 적용한 mariaDB 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './apis/products/products.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/TypeOrmConfigService';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true, // 환경변수 캐싱 설정
+      isGlobal: true, // 환경변수 전역 설정
+    }),
+    TypeOrmModule.forRootAsync({
+      // TypeORM 설정 비동기 처리
+      useClass: TypeOrmConfigService,
+    }),
+    ProductsModule,
+  ],
   controllers: [],
   providers: [],
 })
